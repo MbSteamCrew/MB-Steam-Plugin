ab6dc573d0dc4a091ce568f445600fa9bd83497c
TheMushyPeas <callum.bloomfield01@gmail.com>
2014-07-31 21:13:17 +0100

    Uploaded Base Files
    Uploaded the base files for the server plugin. I am not quite sure what it will do yet, but we will see :D.
    
    @MrCat, maybe this can be a time that you can start to learn to make a server plugin :D

---
+++ com/mbsteam/steamplugin/PluginMain.java
@@ -0,0 +1,23 @@
+import java.util.logging.Level;
+import com.mbserver.api.MBServerPlugin;
+import com.mbserver.api.Manifest;
+
+@Manifest( name = "MbSteamPlugin", authors = "MBSteamCrew")
+
+
+public class PluginMain extends MBServerPlugin {
+
+    @Override
+    public void onEnable() {
+    
+        //Todo - Stuff to do when the server starts up
+    
+      }
+    @Override
+    public void onDisable() {
+    
+        //Todo - Stuff to do when the server shuts down
+    
+    }
+    
+}
